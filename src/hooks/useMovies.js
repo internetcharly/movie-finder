18560be83485935c96ba9722f530a85aee022cde
Carlos Agundez <internetcharlytech@gmail.com>
2023-02-13 11:36:02 +0100

    refactor useMovies custom hook and API services

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,28 +1,12 @@
 import { useState } from 'react'
-import withoutResults from '../mocks/no-results.json'
-import withResults from '../mocks/with-results.json'
+import { searchMovies } from '../services/movies'
 
 export function useMovies({ search }) {
-	const [responseMovies, setResponseMovies] = useState([])
+	const [movies, setMovies] = useState([])
 
-	const movies = responseMovies.Search
-	const mappedMovies = movies?.map((movie) => ({
-		id: movie.imdbID,
-		title: movie.Title,
-		year: movie.Year,
-		poster: movie.Poster,
-	}))
-
-	const getMovies = () => {
-		if (search) {
-			fetch(`https://www.omdbapi.com/?apikey=b0d9b793&s=${search}`)
-				.then((res) => res.json())
-				.then((json) => {
-					setResponseMovies(json)
-				})
-		} else {
-			setResponseMovies(withoutResults)
-		}
+	const getMovies = async () => {
+		const newMovies = await searchMovies({ search })
+		setMovies(newMovies)
 	}
-	return { movies: mappedMovies, getMovies }
+	return { movies, getMovies }
 }
